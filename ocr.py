47061c6ff83f02134c15b05365b34a0cdbeabdf7
Jon Weatherspoon <jon@Jons-MacBook-Pro.local>
2018-12-20 02:45:20 -0800

    Crop position from screenshot

--- ocr.py
+++ ocr.py
@@ -1,10 +1,12 @@
-from PIL import Image
-import pytesseract
-import argparse 
-import cv2
-import os 
 
-FILENAME = "screenshot.jpg"
+import matplotlib.pyplot as plt 
+import numpy as np 
 
-image = cv2.imread(FILENAME)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def crop_position(img_data):
+    COLS = [1700,1860]
+    ROWS = [90,105]
+    return np.array([row[COLS[0]:COLS[1]] for row in img_data[ROWS[0]:ROWS[1]]])
+
+data = plt.imread('screenshot.jpg')
+cropped = crop_position(data)
+plt.imsave('position.png', cropped)
